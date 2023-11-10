3646b86f4d8b031c253cfac886f0618c0b7db1b9
sammanamgain <lionalmessi609@gmail.com>
2023-11-10 20:42:15 +0545

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import Contact from './Pages/Contact';
-import Home from './Pages/Home'
-import {  Routes, Route } from "react-router-dom";
-import React from 'react';
-import Project from './Pages/Project';
-import About from './Pages/About';
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import Project from "./Pages/Project";
+import About from "./Pages/About";
 function App() {
-
-
   return (
-  
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/project' element={<Project/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-      </Routes>
-
-    
-  )
+    <Routes>
+      <Route path="/PORTFOLIO" element={<Home />}></Route>
+      <Route path="/PORTFOLIO/contact" element={<Contact />}></Route>
+      <Route path="/PORTFOLIO/project" element={<Project />}></Route>
+      <Route path="/PORTFOLIO/about" element={<About />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
